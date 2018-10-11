ea199cf737b57056952971b17414a405c651bf9a
Alena Dzhukich <github@alena.moe>
2018-10-12 00:07:01 +0300

    Add initial example && src

---
+++ example/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+import ExampleComponent from 'react-takka-select'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <ExampleComponent/>
+      </div>
+    )
+  }
+}
