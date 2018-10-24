1e671f10ceb7d381b5618bb5841e871e1590c844
Alena Dzhukich <github@alena.moe>
2018-10-24 22:24:09 +0300

    Fix imports

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,3 @@
-export { Indicator } from './components'
-export { Indicators } from './components'
-
-export { Input } from './components'
-export { InputWrapper } from './components'
-
-export { Option } from './components'
-export { Options } from './components'
+export * from './components'
 
 export { default } from './Select'
